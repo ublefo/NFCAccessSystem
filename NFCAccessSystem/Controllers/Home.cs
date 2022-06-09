aba5a62516c93dc7ef0e38665d2ca4116eb9fa4b
ublefo <90136978+ublefo@users.noreply.github.com>
2022-06-09 21:40:20 +1000

    Add http basic auth

--- NFCAccessSystem/Controllers/Home.cs
+++ NFCAccessSystem/Controllers/Home.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using QRCoder;
 
 namespace NFCAccessSystem.Controllers
 {
+    [Authorize]
     public class Home : Controller
     {
         private readonly AccessSystemContext _context;
@@ -21,6 +23,7 @@ namespace NFCAccessSystem.Controllers
         }
 
         // GET: Home
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             return _context.Users != null
--- NFCAccessSystem/Program.cs
+++ NFCAccessSystem/Program.cs
@@ -4,6 +4,10 @@ using Microsoft.EntityFrameworkCore.Sqlite;
 using NFCAccessSystem.Data;
 using NFCAccessSystem.Models;
 using OtpNet;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using idunno.Authentication.Basic;
+using Microsoft.AspNetCore.Authorization;
 
 using var db = new AccessSystemContext();
 Console.WriteLine($"Database path: {db.DbPath}.");
@@ -12,6 +16,55 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AccessSystemContext>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(BasicAuthenticationDefaults.AuthenticationScheme)
+    .AddBasic(options =>
+    {
+        options.Realm = "Basic Authentication";
+        options.Events = new BasicAuthenticationEvents
+        {
+            OnValidateCredentials = context =>
+            {
+                User authenticatingUser;
+                // find user by UID
+                try
+                {
+                    authenticatingUser = db.Users.Single(u => u.TagUid == context.Username);
+                }
+                catch (Exception e)
+                {
+                    // if no user found just return
+                    Console.WriteLine("Basic auth: no user found.");
+                    Console.WriteLine(e);
+                    return Task.CompletedTask;
+                }
+
+                var totp = new Totp(Base32Encoding.ToBytes(authenticatingUser.TotpSecret));
+                long timeWindowUsed;
+
+                Console.WriteLine("ID: " + context.Username);
+                Console.WriteLine("TOTP Provided: " + context.Password);
+
+                if (totp.VerifyTotp(context.Password, out timeWindowUsed, VerificationWindow.RfcSpecifiedNetworkDelay))
+                {
+                    Console.WriteLine("Auth Successful.");
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, context.Username, ClaimValueTypes.String,
+                            context.Options.ClaimsIssuer),
+                        new Claim(ClaimTypes.Name, context.Username, ClaimValueTypes.String,
+                            context.Options.ClaimsIssuer)
+                    };
+
+                    context.Principal = new ClaimsPrincipal(new ClaimsIdentity(claims, context.Scheme.Name));
+                    context.Success();
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -28,10 +81,15 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// TODO: test code, remove after testing
+app.MapGet("/authenticate", [Authorize](ClaimsPrincipal user) => $"Hello {user!.Identity!.Name}");
+app.MapGet("/unauthorized", () => Results.Unauthorized());
 
 app.Run();
