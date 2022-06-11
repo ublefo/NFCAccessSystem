584ff96cee26e7e779b972a0d42ea728fd6c406c
ublefo <90136978+ublefo@users.noreply.github.com>
2022-06-11 10:40:23 +1000

    Update routing & remove controller name from path

--- NFCAccessSystem/Program.cs
+++ NFCAccessSystem/Program.cs
@@ -82,7 +82,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{action=Index}/{id?}",
+    defaults: new {controller = "Home"});
 
 // TODO: test code, remove after testing
 app.MapGet("/authenticate", [Authorize](ClaimsPrincipal user) => $"Hello {user!.Identity!.Name}");
