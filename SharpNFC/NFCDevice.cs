ddf77f1c7191151b601fecb85f705f3b99f5ac2b
ublefo <90136978+ublefo@users.noreply.github.com>
2022-06-14 22:56:10 +1000

    Implment function to read card info

--- SharpNFC/NFCDevice.cs
+++ SharpNFC/NFCDevice.cs
@@ -39,6 +39,32 @@ namespace SharpNFC
             return intResult;
         }
 
+        public nfc_target SelectPassive14443ATarget()
+        {
+            var target = new nfc_target();
+            IntPtr targetPtr = Marshal.AllocHGlobal(Marshal.SizeOf(target));
+
+            try
+            {
+                var nm = new nfc_modulation();
+                nm.nmt = nfc_modulation_type.NMT_ISO14443A;
+                nm.nbr = nfc_baud_rate.NBR_106;
+
+                Marshal.StructureToPtr<nfc_target>(target, targetPtr, false);
+                Functions.nfc_initiator_select_passive_target(DevicePointer, nm, null, 0, targetPtr);
+
+                var updatedTarget = Marshal.PtrToStructure<nfc_target>(targetPtr);
+                return updatedTarget;
+            }
+            finally
+            {
+                if (targetPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(targetPtr);
+                }
+            }
+        }
+
         public void Dispose()
         {
             SharpNFC.PInvoke.Functions.nfc_close(DevicePointer);
