215502a8ec0009ccdf8fbb90a26fbe5be74171b3
ublefo <90136978+ublefo@users.noreply.github.com>
2022-06-15 16:55:21 +1000

    Improve NFCHelper and reduce logging

--- NFCHelper/NFCHelper.cs
+++ NFCHelper/NFCHelper.cs
@@ -4,33 +4,40 @@ namespace NFCHelper;
 
 public class NFCHelper
 {
+    private NFCContext NfcContext { get; set; }
+    private NFCDevice NfcReader { get; set; }
+
+    public NFCHelper()
+    {
+        NfcContext = new NFCContext();
+        var nfcReaderName = NfcContext.ListDeviceNames().FirstOrDefault();
+
+        if (nfcReaderName == null)
+        {
+            Console.WriteLine("No NFC Reader found!");
+            NfcReader = null;
+        }
+
+        Console.WriteLine("Using NFC Reader: " + nfcReaderName);
+        NfcReader = NfcContext.OpenDevice(nfcReaderName);
+    }
+
     public string ReadUid()
     {
-        using (var NfcContext = new NFCContext())
+        if (NfcReader == null)
         {
-            var nfcReaderName = NfcContext.ListDeviceNames().FirstOrDefault();
-
-            if (nfcReaderName == null)
-            {
-                Console.WriteLine("No NFC Reader found!");
-                return null;
-            }
-
-            Console.WriteLine("Using NFC Reader: " + nfcReaderName);
-
-            using (var NfcReader = NfcContext.OpenDevice(nfcReaderName))
-            {
-                var nfcTarget = NfcReader.SelectPassive14443ATarget();
-                if (nfcTarget.nti.szUidLen == 0)
-                {
-                    Console.WriteLine("No card found.");
-                    return null;
-                }
-
-                byte[] uidArray = nfcTarget.nti.abtUid[4 .. 8];
-                Console.WriteLine("UID is: " + BitConverter.ToString(uidArray));
-                return Convert.ToHexString(uidArray, 0, 4);
-            }
+            return null;
         }
+
+        var nfcTarget = NfcReader.SelectPassive14443ATarget();
+        if (nfcTarget.nti.szUidLen == 0)
+        {
+            // Console.WriteLine("No card found.");
+            return null;
+        }
+
+        byte[] uidArray = nfcTarget.nti.abtUid[4 .. 8];
+        Console.WriteLine("UID is: " + BitConverter.ToString(uidArray));
+        return Convert.ToHexString(uidArray, 0, 4);
     }
 }
